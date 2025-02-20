91693e37d91b8c97c30d2e7f346cc8d3a14653aa
Dasha Ravilova <dararavilova@gmail.com>
2025-02-20 23:33:03 +0300

    Функция с поэлементным заполнением списка

--- generateList_fun.py
+++ generateList_fun.py
@@ -1,4 +1,11 @@
 import random
 def generate_list(n):
+<<<<<<< HEAD
     return [random.randint(1, 100) for i in range(n)]
+=======
+    result = []
+    for i in range(n):
+        result.append(random.randint(1, 100))
+    return result
+>>>>>>> a35e432 (Функция с поэлементным заполнением списка)
 print(generate_list(20))
